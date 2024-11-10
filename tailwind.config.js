5328f0c3461873a701f76b165ee39e80b5187dc9
João Álvaro <joaosilvaabc1@gmail.com>
2024-11-10 20:46:39 -0300

    ✨ feat: Introdução

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        oxygen: ["Oxygen", "serif"],
+      },
+    },
   },
   plugins: [],
 }
